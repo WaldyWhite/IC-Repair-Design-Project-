e2781ccb0fe99857edef0a39252aa47b0b12c02a
WaldyWhite <w.weis@hotmail.com>
2023-05-28 23:23:45 +0200

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,18 @@
+new Swiper('.swiper', {
+    slidesPerView: 1,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
